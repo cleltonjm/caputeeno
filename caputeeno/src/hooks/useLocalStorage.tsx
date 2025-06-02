d67d839e723bd6b36347e685eb25e7ae24775faa
Clelton <cleltonjm_95@hotmail.com>
2025-06-02 13:08:07 -0300

    feat: add navigation to product

--- caputeeno/src/hooks/useLocalStorage.tsx
+++ caputeeno/src/hooks/useLocalStorage.tsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
 
-export function useLocalStorage<T>(item: string, initialValue: T){
-    const [value, setValue] = useState<T>(initialValue)
+export function useLocalStorage<T>(item: string, initialValue: T) {
+    const [value, setValue] = useState<T>(initialValue);
 
     useEffect(() => {
         if (typeof window === 'undefined') return;
-        let value = localStorage.getItem(item)
-        if(value) setValue(JSON.parse(value))
-    }, [window])
+
+        const storedValue = localStorage.getItem(item);
+        if (storedValue) {
+            setValue(JSON.parse(storedValue));
+        }
+    }, [item]);
 
     const updateLocalStorage = (newValue: T) => {
         setValue(newValue);
-        localStorage.setItem(item,JSON.stringify(newValue));
-    }
+        if (typeof window !== 'undefined') {
+            localStorage.setItem(item, JSON.stringify(newValue));
+        }
+    };
 
     return {
         value,
         updateLocalStorage
-    }
+    };
 }
